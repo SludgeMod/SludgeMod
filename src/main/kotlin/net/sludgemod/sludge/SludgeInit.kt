b8bdde472f98ef86a4865e716c9554dd02f78c7b
Hannes Feldt <hannes.feldt@outlook.com>
2020-05-02 19:24:54 +0200

    Implements #3

--- src/main/kotlin/net/sludgemod/sludge/SludgeInit.kt
+++ src/main/kotlin/net/sludgemod/sludge/SludgeInit.kt
@@ -1,23 +1,50 @@
 package net.sludgemod.sludge
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.block.FabricBlockSettings
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder
-import net.minecraft.item.ItemGroup
-import net.minecraft.item.ItemStack
+import net.minecraft.block.Blocks
+import net.minecraft.block.FluidBlock
+import net.minecraft.item.*
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
+import net.sludgemod.sludge.shared.fluids.FluidManager
+import net.sludgemod.sludge.shared.fluids.SludgeFluid
 import net.sludgemod.sludge.shared.items.SludgeItem
 
 @Suppress("unused")
 object SludgeInit : ModInitializer {
-
     private const val MOD_ID = "sludge"
+    //Item groups
     val SLUDGE_ITEM_GROUP: ItemGroup = FabricItemGroupBuilder.create(Identifier(MOD_ID, "general"))
             .icon { ItemStack(SLUDGE_ITEM) }
             .build()
+
+    //Fluids
+    val STILL_SLUDGE = SludgeFluid.Still()
+    val FLOWING_SLUDGE = SludgeFluid.Flowing()
+
+    //Items
     private val SLUDGE_ITEM = SludgeItem()
+    val SLUDGE_BUCKET = BucketItem(STILL_SLUDGE, Item.Settings().group(SLUDGE_ITEM_GROUP).recipeRemainder(Items.BUCKET).maxCount(1))
+
+    //Blocks
+    val SLUDGE_FLUID_BLOCK = object: FluidBlock(STILL_SLUDGE, FabricBlockSettings.copy(Blocks.WATER).build()) {}
 
     override fun onInitialize() {
         Registry.register(Registry.ITEM, Identifier(MOD_ID, "sludge_item"), SLUDGE_ITEM)
+        Registry.register(Registry.ITEM, Identifier(MOD_ID, "sludge_bucket"), SLUDGE_BUCKET)
+
+        Registry.register(Registry.FLUID, Identifier(MOD_ID, "sludge"), STILL_SLUDGE)
+        Registry.register(Registry.FLUID, Identifier(MOD_ID, "flowing_sludge"), FLOWING_SLUDGE)
+
+        Registry.register( Registry.BLOCK, Identifier(MOD_ID, "sludge"), SLUDGE_FLUID_BLOCK)
+
+        FluidManager.setupFluidRendering(
+            STILL_SLUDGE,
+            FLOWING_SLUDGE,
+            Identifier("minecraft", "water"),
+            0x964b13
+        )
     }
 }
---
+++ src/main/kotlin/net/sludgemod/sludge/shared/fluids/FluidManager.kt
@@ -0,0 +1,80 @@
+package net.sludgemod.sludge.shared.fluids
+
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandler
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry
+import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.texture.Sprite
+import net.minecraft.client.texture.SpriteAtlasTexture
+import net.minecraft.fluid.Fluid
+import net.minecraft.fluid.FluidState
+import net.minecraft.resource.ResourceManager
+import net.minecraft.resource.ResourceType
+import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.registry.Registry
+import net.minecraft.world.BlockRenderView
+
+object FluidManager {
+    fun setupFluidRendering(
+        still: Fluid,
+        flowing: Fluid,
+        textureFluidId: Identifier,
+        color: Int
+    ) {
+        val stillSpriteId = Identifier(
+            textureFluidId.namespace,
+            "block/" + textureFluidId.path.toString() + "_still"
+        )
+        val flowingSpriteId = Identifier(
+            textureFluidId.namespace,
+            "block/" + textureFluidId.path.toString() + "_flow"
+        )
+
+        // If they're not already present, add the sprites to the block atlas
+        ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEX)
+            .register(ClientSpriteRegistryCallback { _: SpriteAtlasTexture?, registry: ClientSpriteRegistryCallback.Registry ->
+                registry.register(stillSpriteId)
+                registry.register(flowingSpriteId)
+            })
+
+        val fluidId = Registry.FLUID.getId(still)
+        val listenerId =
+            Identifier(fluidId.namespace, fluidId.path + "_reload_listener")
+
+        val fluidSprites = arrayOf<Sprite?>(null, null)
+
+        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES)
+            .registerReloadListener(object : SimpleSynchronousResourceReloadListener {
+                override fun getFabricId(): Identifier {
+                    return listenerId
+                }
+
+                /**
+                 * Get the sprites from the block atlas when resources are reloaded
+                 */
+                override fun apply(resourceManager: ResourceManager?) {
+                    val atlas: java.util.function.Function<Identifier, Sprite> =
+                        MinecraftClient.getInstance().getSpriteAtlas(SpriteAtlasTexture.BLOCK_ATLAS_TEX)
+                    fluidSprites[0] = atlas.apply(stillSpriteId)
+                    fluidSprites[1] = atlas.apply(flowingSpriteId)
+                }
+            })
+
+        // The FluidRenderer gets the sprites and color from a FluidRenderHandler during rendering
+        val renderHandler: FluidRenderHandler = object : FluidRenderHandler {
+            override fun getFluidSprites(view: BlockRenderView, pos: BlockPos, state: FluidState): Array<Sprite?> {
+                return fluidSprites
+            }
+
+            override fun getFluidColor(view: BlockRenderView, pos: BlockPos, state: FluidState): Int {
+                return color
+            }
+        }
+
+        FluidRenderHandlerRegistry.INSTANCE.register(still, renderHandler)
+        FluidRenderHandlerRegistry.INSTANCE.register(flowing, renderHandler)
+    }
+}
