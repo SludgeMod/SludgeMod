dd3aae093a67aee1c0e232fa01839c9e27b7546f
Hannes Feldt <hannes.feldt@outlook.com>
2020-05-12 01:14:51 +0200

    SludgeFluid kotlin functions minification

--- src/main/kotlin/net/sludgemod/sludge/shared/fluids/SludgeFluid.kt
+++ src/main/kotlin/net/sludgemod/sludge/shared/fluids/SludgeFluid.kt
@@ -6,7 +6,6 @@ import net.minecraft.block.FluidBlock
 import net.minecraft.fluid.FlowableFluid
 import net.minecraft.fluid.Fluid
 import net.minecraft.fluid.FluidState
-import net.minecraft.item.Item
 import net.minecraft.state.StateManager
 import net.minecraft.tag.FluidTags
 import net.minecraft.util.math.BlockPos
@@ -23,33 +22,19 @@ abstract class SludgeFluid : FlowableFluid() {
         return Blocks.SLUDGE_FLUID_BLOCK.defaultState.with(FluidBlock.LEVEL, method_15741(state))
     }
 
-    override fun getBucketItem(): Item {
-        return Items.SLUDGE_BUCKET
-    }
+    override fun getBucketItem() = Items.SLUDGE_BUCKET
 
-    override fun getLevelDecreasePerBlock(world: WorldView): Int {
-        return 1
-    }
+    override fun getLevelDecreasePerBlock(world: WorldView) = 1
 
-    override fun matchesType(fluid: Fluid): Boolean {
-        return fluid == still || fluid == flowing
-    }
+    override fun matchesType(fluid: Fluid) = fluid == still || fluid == flowing
 
-    override fun getTickRate(world: WorldView): Int {
-        return 5
-    }
+    override fun getTickRate(world: WorldView) = 5
 
-    override fun getFlowing(): Fluid {
-        return Fluids.FLOWING_SLUDGE
-    }
+    override fun getFlowing() = Fluids.FLOWING_SLUDGE
 
-    override fun isInfinite(): Boolean {
-        return true
-    }
+    override fun isInfinite() = true
 
-    override fun getFlowSpeed(world: WorldView): Int {
-        return 3
-    }
+    override fun getFlowSpeed(world: WorldView) = 3
 
     override fun canBeReplacedWith(
         state: FluidState,
@@ -57,17 +42,11 @@ abstract class SludgeFluid : FlowableFluid() {
         pos: BlockPos,
         fluid: Fluid,
         direction: Direction
-    ): Boolean {
-        return direction == Direction.DOWN && !fluid.isIn(FluidTags.WATER)
-    }
+    ) = direction == Direction.DOWN && !fluid.isIn(FluidTags.WATER)
 
-    override fun getBlastResistance(): Float {
-        return 100.0F
-    }
+    override fun getBlastResistance() = 100.0F
 
-    override fun getStill(): Fluid {
-        return Fluids.STILL_SLUDGE
-    }
+    override fun getStill() = Fluids.STILL_SLUDGE
 
     override fun beforeBreakingBlock(world: IWorld, pos: BlockPos, state: BlockState) {
         val blockEntity = if (state.block.hasBlockEntity()) world.getBlockEntity(pos) else null
@@ -80,22 +59,14 @@ abstract class SludgeFluid : FlowableFluid() {
             builder.add(FlowableFluid.LEVEL)
         }
 
-        override fun getLevel(state: FluidState): Int {
-            return state.get(FlowableFluid.LEVEL) as Int
-        }
+        override fun getLevel(state: FluidState): Int = state.get(FlowableFluid.LEVEL)
 
-        override fun isStill(state: FluidState): Boolean {
-            return false
-        }
+        override fun isStill(state: FluidState) = false
     }
 
     class Still : SludgeFluid() {
-        override fun getLevel(state: FluidState): Int {
-            return 8
-        }
+        override fun getLevel(state: FluidState) = 8
 
-        override fun isStill(state: FluidState): Boolean {
-            return true
-        }
+        override fun isStill(state: FluidState) = true
     }
 }
